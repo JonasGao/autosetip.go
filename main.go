537f548ca44ea285158b182ee46714b23014be59
Jonas Gao <gaopinsong@live.cn>
2024-04-22 00:34:48 +0800

    Implement call api

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	openapi "github.com/alibabacloud-go/darabonba-openapi/client"
+	ecs20140526 "github.com/alibabacloud-go/ecs-20140526/v4/client"
+	util "github.com/alibabacloud-go/tea-utils/service"
+	"github.com/alibabacloud-go/tea/tea"
 	"gopkg.in/yaml.v2"
 	"io"
 	"net/http"
@@ -11,12 +15,57 @@ import (
 type AliyunTarget struct {
 	AccessKey       string `yaml:"access_key"`
 	SecretKey       string `yaml:"secret_key"`
+	Region          string `yaml:"region"`
+	Endpoint        string `yaml:"endpoint,omitempty"`
 	SecurityGroupId string `yaml:"security_group_id"`
 }
 
 type Config struct {
 	IpApiURL string         `yaml:"ip_api_url,omitempty"`
 	Aliyun   []AliyunTarget `yaml:"aliyun"`
+}
+
+func createClient(target AliyunTarget) (_result *ecs20140526.Client, _err error) {
+	c := &openapi.Config{
+		AccessKeyId:     tea.String(target.AccessKey),
+		AccessKeySecret: tea.String(target.SecretKey),
+	}
+	c.Endpoint = tea.String(target.Endpoint)
+	_result, _err = ecs20140526.NewClient(c)
+	return _result, _err
+}
+
+func modifyIp(target AliyunTarget, ip string) error {
+	client, err := createClient(target)
+	if err != nil {
+		return err
+	}
+	req := &ecs20140526.ModifySecurityGroupRuleRequest{
+		RegionId:        tea.String(target.Region),
+		SecurityGroupId: tea.String(target.SecurityGroupId),
+		Policy:          tea.String("accept"),
+		Priority:        tea.String("100"),
+		IpProtocol:      tea.String("tcp"),
+		SourceCidrIp:    tea.String(ip),
+		PortRange:       tea.String("22"),
+		Description:     tea.String("Auto create by autosetip.go"),
+	}
+	runtime := &util.RuntimeOptions{}
+	err = func() (_e error) {
+		defer func() {
+			if r := tea.Recover(recover()); r != nil {
+				_e = r
+			}
+		}()
+		// 复制代码运行请自行打印 API 的返回值
+		_, err = client.ModifySecurityGroupRuleWithOptions(req, runtime)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}()
+	return err
 }
 
 func readConfig() (Config, error) {
@@ -36,6 +85,11 @@ func readConfig() (Config, error) {
 	}
 	if config.IpApiURL == "" {
 		config.IpApiURL = defaultApi
+	}
+	for _, target := range config.Aliyun {
+		if target.Endpoint == "" {
+			target.Endpoint = "ecs" + target.Region + ".aliyuncs.com"
+		}
 	}
 	return config, nil
 }
@@ -69,5 +123,10 @@ func main() {
 	if err != nil {
 		fmt.Printf("Fetch ip error: %v\n", err)
 	}
-	fmt.Println(ip)
+	for _, target := range config.Aliyun {
+		err = modifyIp(target, ip)
+		if err != nil {
+			fmt.Printf("Modify ip error: %v\n", err)
+		}
+	}
 }
