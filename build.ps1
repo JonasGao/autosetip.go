4eada04431520863439cdf6bc4a7614544644585
Jonas Gao <gaopinsong@live.cn>
2024-07-04 16:52:59 +0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -4,15 +4,22 @@ $name = $args[2]
 Write-Output "Using" $file
 $file = (Get-Item $file).FullName
 Push-Location .\encrypt
+Write-Output "Building encrypt"
 go build main.go
+Write-Output "Encrypting"
 ./main.exe $pass $file
 Remove-Item main.exe
 Pop-Location
+Write-Output "Config one"
 Move-Item encrypt\config.yaml.enc one\static\
 Push-Location .\one
+Write-Output "Building one"
 go build
+Write-Output "Compressing"
 Compress-Archive one.exe one.zip
+Write-Output "Clear"
 Remove-Item static\config.yaml.enc
 Remove-Item one.exe
 Move-Item one.zip ..\$name.zip
 Pop-Location
+Write-Output "Finish"
